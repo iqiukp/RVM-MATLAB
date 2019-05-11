d17663eef823eb49ffc4bf869272820000d0b6cf
iqiukp <44639227+iqiukp@users.noreply.github.com>
2019-05-11 14:58:59 +0800

    Update demo.m

--- demo.m
+++ demo.m
@@ -1,4 +1,10 @@
-% demo
+%%%%%%%%%%      Relevance Vector Machine (RVM)              %%%%%%%%%%%%%%%
+% Demo: prediction for a numerical example by using RVM
+% Improve the performance by adjusting the following parameters:
+% 1. sigma   % kernel width
+%
+% ---------------------------------------------------------------------%
+
 clc
 clear all
 close all
@@ -6,49 +12,27 @@ addpath(genpath(pwd))
 
 % sinc funciton
 fun = @(x) sin(abs(x))/abs(x);
+
 % training samples
 x = linspace(-10,10,100);
 y = arrayfun(fun,x);
-X = x';
-Y = y';
-% test samples
-xtest = linspace(-10,10,20);
+Xtrain = x';
+Ytrain = y';
+
+% testing samples
+xtest = linspace(-10,10,30);
 ytest = arrayfun(fun,xtest);
 Xtest = xtest';
 Ytest = ytest';
 
-% train RVM model
-[model]= rvm_train(X,Y);
+% Train RVM model
+model = rvm_train(Xtrain,Ytrain,'sigma',5.5,'bias',1);
 
-% test  RVM model
+% Test RVM model
 [y_mu,y_var] = rvm_test(model,Xtest);
 
-%
-ypre = y_mu;
+% Plot the training results 
+plottrainingResult(Xtrain,Ytrain,model)
 
-% 
-figure
-plot(X,Y,':o','LineWidth',1.5,'MarkerSize',4)
-hold on
-plot(X,model.y_mu,':o','LineWidth',1.5,'MarkerSize',4)
-plot(X(model.rv_index),Y(model.rv_index),'go', ... 
-    'LineWidth',1.5,'MarkerSize',8)
-
-legend('training samples','prediction','relevance Vectors')
-
-figure
-xs = (1:size(Xtest,1))';
-% 3¦Ò
-f1 = [y_mu(:,1)+2*sqrt(y_var(:,1)); flip(y_mu(:,1)-2*sqrt(y_var(:,1)),1)];
-fill([xs; flip(xs,1)], f1, [7 7 7]/8)
-hold on
-plot(Ytest,'b:o','LineWidth',1.5,'MarkerSize',4)
-plot(y_mu,'r:o','LineWidth',1.5,'MarkerSize',4)
-
-xlabel('Samples')
-ylabel('Values')
-legend('3¦Ò boundary','test samples','prediction')
-
-
-
-
+% Plot the testing results 
+plottestingResult(Xtest,Ytest,y_mu,y_var)
