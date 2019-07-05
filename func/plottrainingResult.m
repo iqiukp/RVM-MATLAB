ae6925fd63f3374b5ef495b574dcdcbb51fc87e3
iqiukp <44639227+iqiukp@users.noreply.github.com>
2019-07-05 21:41:55 +0800

    Update plottrainingResult.m

--- func/plottrainingResult.m
+++ func/plottrainingResult.m
@@ -9,19 +9,25 @@ function plottrainingResult(x,y,model)
 %   y         training data
 %   model     training model
 %
-% Created on 11st May 2019, by Kepeng Qiu.
+% Created on 5th July 2019, by Kepeng Qiu.
 %-------------------------------------------------------------%
 
-%
+
+rv_index = model.rv_index;
+
+% Delete the index of bias
+if model.bias
+    rv_index(model.rv_index == model.bias_index) = [];
+end
 
 figure
 hold on
 grid on
 
-plot(x,y,'b:o','LineWidth',1,'MarkerSize',3, ... 
+plot(x,y,'b:o','LineWidth',1,'MarkerSize',3, ...
     'MarkerEdgeColor', 'b', 'MarkerFaceColor', 'b')
 
-plot(x(model.rv_index),y(model.rv_index),'ro','MarkerSize',8, ... 
+plot(x(rv_index),y(rv_index),'ro','MarkerSize',8, ...
     'MarkerEdgeColor', 'r')
 
 % axis settings
@@ -33,12 +39,14 @@ set(gca,'FontSize',tgca,'FontName',tfont)
 
 % legend settings
 tlegend = tgca*0.9;
-legend({'training samples','relevance vectors'},'FontSize',tlegend , ... 
+legend({'training samples','relevance vectors'},'FontSize',tlegend , ...
     'FontWeight','normal','FontName',tfont)
 
 % label settings
-tlabel = tgca*1.1; 
-xlabel('Samples','FontSize',tlabel,'FontWeight','normal','FontName',tfont,'Color','k')
-ylabel('Value','FontSize',tlabel,'FontWeight','normal','FontName',tfont,'Color','k')
+tlabel = tgca*1.1;
+xlabel('Samples','FontSize',tlabel,'FontWeight','normal', ...
+    'FontName',tfont,'Color','k')
+ylabel('Value','FontSize',tlabel,'FontWeight','normal', ...
+    'FontName',tfont,'Color','k')
 
 end
