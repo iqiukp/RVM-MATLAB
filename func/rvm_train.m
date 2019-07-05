0804eb0703d8eb3d91cda032344401eb034504db
iqiukp <44639227+iqiukp@users.noreply.github.com>
2019-07-05 21:42:08 +0800

    Update rvm_train.m

--- func/rvm_train.m
+++ func/rvm_train.m
@@ -1,27 +1,27 @@
-function [model]= rvm_train(X,Y,varargin)
+function model = rvm_train(X,Y,varargin)
 % DESCRIPTION
 % Prediction based on Relevance Vector Machine (RVM)
 % Using SB2_Release_200 toolbox
 % http://www.miketipping.com/sparsebayes.htm
 %
-%       [model]= rvm_train(X,Y,varargin)
+%       model = rvm_train(X,Y,varargin)
 %
 % INPUT
-%   X            Training samples (N*d)
+%   X            training samples (N*d)
 %                N: number of samples
 %                d: number of features
-%   Y            Target samples (N*1)
+%   Y            target samples (N*1)
 %
 % OUTPUT
 %   model        RVM model
 %
-% Created on 11st May 2019, by Kepeng Qiu.
+% Created on 5th July 2019, by Kepeng Qiu.
 %-------------------------------------------------------------%
 
 % Default Parameters setting
-sigma = 3;       % kernel width
-bias = 0;        % 0: no FreeBasis' --- bias
-                 % 1: add bias
+width = 3;          % kernel width
+bias = 0;           % 0: no free basis (bias)
+                    % 1: add bias
 %
 if rem(nargin,2)
     error('Parameters to rvm_train should be pairs')
@@ -33,10 +33,10 @@ for n =1:numParameters
     value	= varargin{(n-1)*2+2};
     switch Parameters
             %
-        case 'sigma'
-            sigma = value;
+        case 's'
+            width = value;
             %
-        case 'bias'
+        case 'b'
             bias = value;
     end
 end
@@ -45,38 +45,45 @@ end
 N = size(X,1);
 
 % Compute the kernel matrix
-K = computeKM(X,X,sigma);
+K = computeKM(X,X,width);
 
 % Construct the basis vectors
 if ~bias
     % No bias
     BASIS = K;
-    [PARAMETER, HYPERPARAMETER, DIAGNOSTIC] = ... 
+    [PARAMETER, HYPERPARAMETER, ~] = ... 
     SparseBayes('Gaussian', BASIS, Y);
 else
     % Add bias
-    indexBias = N+1;
+    indexBias = N+1;  % index of bias
     BASIS = [K,ones(N,1)];
     OPTIONS = SB2_UserOptions('freeBasis',indexBias);
-    [PARAMETER, HYPERPARAMETER, DIAGNOSTIC] = ... 
+    [PARAMETER, HYPERPARAMETER, ~] = ... 
     SparseBayes('Gaussian', BASIS, Y,OPTIONS);
 end
 
+
 % RVM model
-model.rv_index = PARAMETER.Relevant;
-model.rv_mu = PARAMETER.Value;
-model.width = sigma;
-model.X = X;
-model.beta = HYPERPARAMETER.beta;
-model.sigma = DIAGNOSTIC.Sigma;
-model.bias = PARAMETER.Bias;
+model.rv_index = PARAMETER.Relevant; % index of relevant vetors
+model.bias = bias;                   % bias
+if bias
+    model.bias_index = indexBias;    % index of bias
+    if size(model.rv_index,1) == 1
+        if model.rv_index == indexBias
+            warning('No relevant vetors were found! Please reduce the value of kernel width.')
+        end
+    end
+end
 
-% prediction
-model.y_mu = BASIS(:,model.rv_index)*model.rv_mu+model.bias;
+model.rv_mu = PARAMETER.Value;       % vector of weight values
+model.beta = HYPERPARAMETER.beta;    % noise precision
+model.alpha = HYPERPARAMETER.Alpha;  % vector of weight precision values
+model.width = width;            % kernel width of Gaussian kernel function
+model.X = X;                         % training samples
 
-% variance of prediction (training samples)
-model.y_var = ones(N,1)*model.beta^-1+ ... 
-    diag(BASIS(:,model.rv_index)* ... 
-    model.sigma*BASIS(:,model.rv_index)');
+%
+PHI = BASIS(:,model.rv_index);
+tmp = diag(HYPERPARAMETER.Alpha)+HYPERPARAMETER.beta*(PHI'*PHI);
+model.sigma = inv(tmp);              % posterior covariance ¡Æ
 
 end
