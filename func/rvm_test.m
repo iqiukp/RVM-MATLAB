84dc4640beda7dcb82d124dbb668f9695e6f4070
iqiukp <44639227+iqiukp@users.noreply.github.com>
2020-03-18 22:42:37 +0800

    Update rvm_test.m

--- func/rvm_test.m
+++ func/rvm_test.m
@@ -1,44 +1,62 @@
-function [y_mu,y_var] = rvm_test(model,X)
-% DESCRIPTION
-% Prediction based on Relevance Vector Machine (RVM)
-% Using SB2_Release_200 toolbox
-% http://www.miketipping.com/sparsebayes.htm
-%
-%       [y_mu,y_var] = rvm_test(model,X)
-%
-% INPUT
-%   X            testing samples (n*d)
-%                n: number of samples
-%                d: number of features
-%   model        RVM model%
-%
-% OUTPUT
-%   y_mu         prediction
-%   y_var        variance of prediction
-%
-%
-% Created on 5th July 2019, by Kepeng Qiu.
-%-------------------------------------------------------------%
+function result = rvm_test(model, xt, yt)
+    %{
 
-N = size(X,1);
-% Compute the kernel matrix
-K = computeKM(X,model.X,model.width);
+        DESCRIPTION
 
-% Construct the basis vectors
-if ~model.bias
-    % No bias
-    BASIS = K;
-else
-    % Add bias
-    BASIS = [K,ones(N,1)];
-end
+              result = rvm_test(model, xt, yt)
 
-% prediction
-y_mu = BASIS(:,model.rv_index)*model.rv_mu;
+        INPUT
+          model        RVM model
+          xt           testing samples (n*d)
+                       n: number of samples
+                       d: number of features
+          yt           target samples (n*1)
 
-% variance of prediction
-y_var = ones(N,1)*model.beta^-1+ ... 
-    diag(BASIS(:,model.rv_index)* ... 
-    model.sigma*BASIS(:,model.rv_index)');
 
+        OUTPUT
+          result       predicted results
+
+        Created on 18th March 2020, by Kepeng Qiu.
+        -------------------------------------------------------------
+
+    %}
+    if nargin < 3
+        warning('Please input the real value of the test data')
+    end
+    
+    
+    tic
+    nt = size(xt, 1);
+    % Compute the kernel matrix
+    K = model.kernel.getKernelMatrix(xt, model.rv);
+
+    % Construct the basis vectors
+    if ~strcmp(model.option.freeBasis, 'on')
+        % No bias
+        BASIS = K;
+    else
+        % Add bias
+        BASIS = [K, ones(nt, 1)];
+    end
+    
+    result = struct();
+    % prediction
+    result.ypre = BASIS*model.mu;
+
+    % variance of prediction
+    result.yvar = model.beta^-1+diag(BASIS*model.sigma*BASIS');
+    timeCost = toc;
+    
+    % model evaluation 
+    [result.RMSE, result.CD, result.MAE] = computePretIndex(yt, result.ypre);
+    
+    if strcmp(model.option.display, 'on')
+        fprintf('\n')
+        fprintf('*** RVM model test finished ***\n')
+        fprintf('time cost      =  %.4f s\n', timeCost)
+        fprintf('predicted RMSE =  %.4f\n', result.RMSE)
+        fprintf('predicted CD   =  %.4f\n', result.CD)
+        fprintf('predicted MAE  =  %.4f\n', result.MAE)
+        fprintf('\n')
+    end
 end
