5190b90836201c1efd6297020124c6be4da7d460
iqiukp <44639227+iqiukp@users.noreply.github.com>
2020-03-18 22:41:52 +0800

    Update computePretIndex.m

--- func/computePretIndex.m
+++ func/computePretIndex.m
@@ -1,32 +1,39 @@
-function [RMSE,CD,MAE] = computePretIndex(y,ypre)
-% DESCRIPTION
-% Compute regression performance evaluation index
-%
-%    [x, y, xt, yt] = generateData
-%
-% INPUT
-%   y         true value
-%   ypre      predicted value
-%
-%
-% OUTPUT
-%   RMSE      root-mean-square error
-%   CD        coefficient of determination
-%   MAE       mean absolute error
-%
-% Created on 5th July 2019, by Kepeng Qiu.
-%-------------------------------------------------------------%
+function [RMSE, CD, MAE] = computePretIndex(y, ypre)
+    %{
 
-% RMSE
-RMSE= sqrt(sum((y-ypre).^2)/size(y,1));
+        DESCRIPTION
 
-% CD
-SSR = sum((ypre-mean(y)).^2);
-SSE = sum((y-ypre).^2);
-SST = SSR+SSE;
-CD = 1-SSE./SST;
+        Compute performance evaluation index
 
-% MAE
-MAE = sum(abs(y-ypre))/size(y,1);
+        -------------------------------------------------------------
+
+              [RMSE, CD, MAE] = computePretIndex(y, ypre)
+
+        INPUT
+          y            real value
+          ypre         predicted value
+
+
+        OUTPUT
+          RMSE         root-mean-square error
+          CD           coefficient of determination
+          MAE          mean absolute error
+
+        Created on 18th March 2020, by Kepeng Qiu.
+        -------------------------------------------------------------%
+
+    %}
+
+    % RMSE
+    RMSE= sqrt(sum((y-ypre).^2)/size(y, 1));
+
+    % CD
+    SSR = sum((ypre-mean(y)).^2);
+    SSE = sum((y-ypre).^2);
+    SST = SSR+SSE;
+    CD = 1-SSE./SST;
+
+    % MAE
+    MAE = sum(abs(y-ypre))/size(y, 1);
 
 end
