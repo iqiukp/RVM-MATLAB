6deee3cd262bfcaf9a56309bb8d640a543c695b4
iqiukp <44639227+iqiukp@users.noreply.github.com>
2020-03-18 22:41:54 +0800

    Update generateData.m

--- func/generateData.m
+++ func/generateData.m
@@ -1,33 +1,41 @@
 function [x, y, xt, yt] = generateData
-% DESCRIPTION
-% Generate data using sinc function
-%
-%    [x, y, xt, yt] = generateData
-%
-% OUTPUT
-%   x         trainging data (input)
-%   y         trainging data (output)
-%   xt        testing data (input)
-%   yt        testing data (output)
-%
-% Created on 5th July 2019, by Kepeng Qiu.
-%-------------------------------------------------------------%
+    %{
 
-% sinc funciton
-fun = @(x) sin(abs(x))/abs(x);
-N = 100;
-Nt = 50;
+        DESCRIPTION
 
-% training samples
-x = linspace(-10,10,N);
-y = arrayfun(fun,x);
-x = x';
-y = y';
+        Generate data using sinc function
 
-% testing samples
-xt = linspace(-10,10,Nt);
-yt = arrayfun(fun,xt);
-xt = xt';
-yt = yt';
+        -------------------------------------------------------------
+
+              [x, y, xt, yt] = generateData
+
+        OUTPUT
+          x            trainging input
+          y            trainging output
+          xt           test input
+          yt           test output
+
+        Created on 18th March 2020, by Kepeng Qiu.
+        -------------------------------------------------------------%
+
+    %}
+
+    snr = 18;
+    % sinc funciton
+    fun = @(x) awgn(sin(abs(x))/abs(x), snr);
+    N = 100;
+    Nt = 50;
+
+    % training samples
+    x = awgn(linspace(-10, 10, N), snr);
+    y = arrayfun(fun, x);
+    x = x';
+    y = y';
+
+    % test samples
+    xt= awgn(linspace(-10, 10, Nt), snr);
+    yt = arrayfun(fun, xt);
+    xt = xt';
+    yt = yt';
 
 end
